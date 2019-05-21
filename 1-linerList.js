00d3eb7de93131ea593e776d5b3a13df11480eaf
Moersing <1261870167@qq.com>
2019-05-21 23:46:59 +0800

    'start'

---
+++ 1-linerList.js
@@ -0,0 +1,13 @@
+/***
+ * 1. 链表必须有一个Head
+ * 2. 每个元素必须有一个next和data,分别代表下一个元素和本身, 单项链表最后一个没有next
+ */
+class Element {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+class LinerList {
+
+}
